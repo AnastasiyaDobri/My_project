f9bd5cb0e2a9241b2b8c8fcf8bb8e40e200afe1b
anastasiya_d <a.dobriyan@icloud.com>
2020-06-11 11:19:30 +0300

    new task

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -122,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/AnastasiyaDobri/mysite/static'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/home/AnastasiyaDobri/mysite/media'

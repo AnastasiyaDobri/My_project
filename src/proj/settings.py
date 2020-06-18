517a4e0c90e0b35fc785738f8546e68fe27681b9
anastasiya_d <a.dobriyan@icloud.com>
2020-06-18 15:36:00 +0300

    books-genre view2

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = '/home/AnastasiyaDobri/mysite/static'
 MEDIA_URL = '/media/'
-#MEDIA_ROOT = '/home/AnastasiyaDobri/mysite/media'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/home/AnastasiyaDobri/mysite/My_project/src/media'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'

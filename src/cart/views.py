abe3eed113fb168ba864aa5d04f3f3e14a2a770c
anastasiya_d <a.dobriyan@icloud.com>
2020-07-01 11:46:55 +0300

    cart v1

--- src/cart/views.py
+++ src/cart/views.py
@@ -2,17 +2,20 @@ from django.shortcuts import render
 from django.contrib import admin
 from . import models
 from django.views.generic.edit import UpdateView
-from django.views.generic.detail import DetailView
+from django.views.generic import ListView
 from books.models import Books
 from cart.models import BooksInCart
 from cart.models import Cart
 from .forms import AddBookToCartForm
 from django.urls import reverse_lazy
+from profiles.models import Profiles
+from django.contrib.messages.views import SuccessMessageMixin
 
-class AddBookToCart(UpdateView):
+class AddBookToCart(UpdateView, SuccessMessageMixin):
     model=models.BooksInCart
     template_name='cart/add.html'
     form_class=AddBookToCartForm
+    success_message =f"{book} was added to cart"
     def get_object(self):
         book_pk=self.request.GET.get('book_pk')
         cart_pk=self.request.session.get('cart_pk',0)
@@ -36,9 +39,12 @@ class AddBookToCart(UpdateView):
     def get_success_url(self):
        return reverse_lazy('books:list')
 
-class CartTotal(DetailView):
+class CartTotal(ListView):
     model=BooksInCart
     template_name='cart/cart.html'
-    def get_object(self):
-        cart_pk=self.request.session.get('cart_pk')
-        return cart_pk
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = Profiles.objects.all()
+        return context
+    def get_success_url(self):
+       return reverse_lazy('main')
